5fdf5a315d1ed6b73c3b0746358206ba3f744cf1
Tom Janssen <tom@Toms-MacBook-Pro.fritz.box>
2023-09-13 12:15:22 +0200

    initial commit

---
+++ service-registry/src/test/java/com/backendbuilder/showcase/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendbuilder.showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
