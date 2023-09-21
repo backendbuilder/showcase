3f576e8871986cd77dc0dd3ebf0408d40d4093f8
tom <t.janssen13788@gmail.com>
2023-09-21 14:56:54 +0200

    remove security for actuator endpoint of bank-service

--- api-gateway/src/main/java/com/showcase/configuration/SecurityConfig.java
+++ api-gateway/src/main/java/com/showcase/configuration/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.showcase.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -15,9 +14,10 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        http.cors(ServerHttpSecurity.CorsSpec::disable);
         http.authorizeExchange(authorize -> authorize
-                .pathMatchers(HttpMethod.GET, "/restaurant/public/list").permitAll()
-                .pathMatchers(HttpMethod.GET, "/restaurant/public/menu/*").permitAll()
+                .pathMatchers("/actuator/**").permitAll()
+                .pathMatchers("bank-service/actuator/**").permitAll()
                 .pathMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyExchange().authenticated());
         http.oauth2ResourceServer(t-> t.jwt(Customizer.withDefaults()));
