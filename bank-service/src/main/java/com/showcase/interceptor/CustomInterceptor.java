79bd088d2bd7840445220dcea5ad1e5a07de7be2
tom <t.janssen13788@gmail.com>
2023-10-05 10:56:26 +0200

    add empty interceptors for debugging purposes

---
+++ bank-service/src/main/java/com/showcase/interceptor/CustomInterceptor.java
@@ -0,0 +1,42 @@
+
+package com.showcase.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class CustomInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+       /* SecurityContext securityContext = SecurityContextHolder.getContext();
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (header != null) {
+            String[] authElements = header.split(" ");
+
+            if (authElements.length == 2
+                    && "Bearer".equals(authElements[0])) {
+                securityContext.setAuthentication(new PreAuthenticatedAuthenticationToken(authElements[1], null));
+            }
+        }*/
+    return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
+
